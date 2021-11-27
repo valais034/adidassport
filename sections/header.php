1c5b05acb3143e26e76234b62dff1402a968b1d2
User <valais034@gmail.com>
2021-11-27 14:27:49 +0330

    adidassport

--- sections/header.php
+++ sections/header.php
@@ -38,7 +38,7 @@ function myFunction() {
     <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-
+<!--f-->
     <header class="header-section" >
         <div class="header-bottom">
             <div class="container">
