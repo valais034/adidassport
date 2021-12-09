101325a8a861b8d9cffe78793b8a9e2cd329a12a
User <valais034@gmail.com>
2021-12-09 12:18:40 +0330

    adidassport

---
+++ complete-blog-php/index.php
@@ -0,0 +1,29 @@
+<?php require_once('includes/head_section.php') ?>
+<?php include('includes/navbar.php') ?>
+	<title>LifeBlog | Home </title>
+</head>
+<body>
+	<!-- container - wraps whole page -->
+	<div class="container">
+		<!-- navbar -->
+
+		<!-- // navbar -->
+
+		<!-- Page content -->
+		<div class="content">
+			<h2 class="content-title">Recent Articles</h2>
+			<hr>
+			<!-- more content still to come here ... -->
+		</div>
+		<!-- // Page content -->
+
+		<!-- footer -->
+		<div class="footer">
+			<p>MyViewers &copy; <?php echo date('Y'); ?></p>
+</div>
+<!-- // footer -->
+
+</div>
+<!-- // container -->
+</body>
+</html>
