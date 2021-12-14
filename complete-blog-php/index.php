5f6bd566d1dbe666a2a51930d29122ec981b39bd
User <valais034@gmail.com>
2021-12-14 16:22:15 +0330

    adidassport

--- complete-blog-php/index.php
+++ complete-blog-php/index.php
@@ -1,5 +1,6 @@
 <!-- The first include should be config.php -->
 <?php require_once('config.php') ?>
+<?php require_once( ROOT_PATH . '/includes/registration_login.php') ?>
 <?php require_once( ROOT_PATH . '/includes/public_functions.php') ?>
 
 <!-- Retrieve all posts from database  -->
@@ -28,6 +29,15 @@
         <?php foreach ($posts as $post): ?>
             <div class="post" style="margin-left: 0px;">
                 <img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+                <!-- Added this if statement... -->
+                <?php if (isset($post['topic']['name'])): ?>
+                    <a
+                            href="<?php echo BASE_URL . 'filtered_posts.php?topic=' . $post['topic']['id'] ?>"
+                            class="btn category">
+                        <?php echo $post['topic']['name'] ?>
+                    </a>
+                <?php endif ?>
+
                 <a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
                     <div class="post_info">
                         <h3><?php echo $post['title'] ?></h3>
